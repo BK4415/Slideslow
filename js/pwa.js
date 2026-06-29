11275e6610d221bb11693991127e8d7c67cb679a
BK4415 <bshill443@gmail.com>
2026-06-29 09:34:58 +0530

    Update pwa.js

--- js/pwa.js
+++ js/pwa.js
@@ -1,7 +1,21 @@
+/**
+ * PWA REGISTRATION
+ */
 if ('serviceWorker' in navigator) {
     window.addEventListener('load', () => {
-        navigator.serviceWorker.register('sw.js')
-            .then(reg => console.log('SW Registered'))
-            .catch(err => console.log('SW Failed', err));
+        navigator.serviceWorker.register('./sw.js')
+            .then(registration => {
+                console.log('SW registered: ', registration.scope);
+            })
+            .catch(registrationError => {
+                console.log('SW registration failed: ', registrationError);
+            });
     });
 }
+
+// Handle "Add to Home Screen" prompt
+let deferredPrompt;
+window.addEventListener('beforeinstallprompt', (e) => {
+    e.preventDefault();
+    deferredPrompt = e;
+});
