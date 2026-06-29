54e5a29cf0fb59deafece7ef1a9efb4ebcb200e1
BK4415 <bshill443@gmail.com>
2026-06-29 09:18:25 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,32 @@
-const CACHE_NAME = 'wood-slide-v1';
+const CACHE_NAME = 'premium-wood-slide-v1';
 const ASSETS = [
-    './',
-    './index.html',
-    './css/style.css',
-    './js/engine.js',
-    './js/ui.js',
-    './js/pwa.js'
+    'index.html',
+    'about.html',
+    'privacy.html',
+    'terms.html',
+    'guide.html',
+    'css/style.css',
+    'js/engine.js',
+    'js/ui.js',
+    'js/pwa.js',
+    'json/ratings.json',
+    'assets/images/photo1.jpg',
+    'assets/images/photo2.jpg',
+    'assets/images/photo3.jpg',
+    'assets/images/photo4.jpg',
+    'assets/images/photo5.jpg'
 ];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS)));
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+    );
 });
 
-self.addEventListener('fetch', e => {
-    e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
 });
